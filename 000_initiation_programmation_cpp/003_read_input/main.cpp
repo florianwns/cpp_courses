4a0116e89560c1788193bac6dea3bb07285d67f3
Florian REY <florianrey.info@gmail.com>
2023-02-10 16:12:15 +0100

    read input

---
+++ 000_initiation_programmation_cpp/003_read_input/main.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Florian Rey on 10/02/2023.
+//
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    int square_n;
+
+    cout << "Enter a value for 'n' : ";
+    cin >> n;
+
+    square_n = n * n;
+    cout  << "The square of 'n' is " << square_n << endl ;
+
+    return 0;
+}
