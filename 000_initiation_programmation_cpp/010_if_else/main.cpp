fb4621f809d60b495b8ed7876d7c0ebdcc69ad16
Florian REY <florianrey.info@gmail.com>
2023-02-13 14:29:36 +0100

    trick on if else

---
+++ 000_initiation_programmation_cpp/010_if_else/main.cpp
@@ -0,0 +1,23 @@
+//
+// Created by Florian Rey on 13/02/2023.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    if (-1 and 5) { // OK
+        cout << "the expression '(-1 and 5)' returns  " << (-1 and 5) << endl;
+    }
+    int a(5), b(7);
+    cout << "(a = b) returns " << (a = b) << endl;
+
+    // Becareful here c++ is different than python
+    cout << "(0 <= a <= 2) returns " << (0 <= a <= 2) << endl;
+    cout << "((0 <= a) <= 2) returns " << ((0 <= a) <= 2) << endl;
+
+    cout << "not(7) returns " << not(7) << endl;
+    cout << "not(0) returns " << not(0) << endl;
+    return 0;
+}
