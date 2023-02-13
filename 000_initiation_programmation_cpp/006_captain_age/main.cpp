3c31562f1c3b32ac2179f5d820158848f53cc7e7
Florian REY <florianrey.info@gmail.com>
2023-02-13 11:17:10 +0100

    add const

--- 000_initiation_programmation_cpp/006_captain_age/main.cpp
+++ 000_initiation_programmation_cpp/006_captain_age/main.cpp
@@ -14,9 +14,8 @@ int main() {
     cin >> age;
 
     const std::time_t now = time(nullptr);
-    int current_year(gmtime(&now)->tm_year + 1900);
-
-    int birth_year = current_year - age;
+    const int current_year(gmtime(&now)->tm_year + 1900);
+    const int birth_year = current_year - age;
 
     cout << "You were born in " << birth_year << endl;
     return 0;
