545436d7fb73bdeb0ae572a68dbf8694949f801a
Florian REY <florianrey.info@gmail.com>
2023-02-13 14:50:06 +0100

    add bool to limits

--- 000_initiation_programmation_cpp/008_limits/main.cpp
+++ 000_initiation_programmation_cpp/008_limits/main.cpp
@@ -10,8 +10,9 @@ using namespace std;
 int main() {
 
     cout << "======== Limits ========" << endl
+         << numeric_limits<bool>::min() << " <= bool <= " << numeric_limits<bool>::max() << endl
          << (int) numeric_limits<uint8_t>::min() << " <= uint8_t <= " << (int) numeric_limits<uint8_t>::max() << endl
-         <<  (int)numeric_limits<char>::min() << " <= char <= " <<  (int)numeric_limits<char>::max() << endl
+         << (int) numeric_limits<char>::min() << " <= char <= " << (int) numeric_limits<char>::max() << endl
          << numeric_limits<uint16_t>::min() << " <= uint16_t <= " << numeric_limits<uint16_t>::max() << endl
          << numeric_limits<uint32_t>::min() << " <= uint32_t <= " << numeric_limits<uint32_t>::max() << endl
          << numeric_limits<float>::min() << " <= float <= " << numeric_limits<float>::max() << endl
