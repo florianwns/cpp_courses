cb69d530fa13f15b67a8a15554b3a8e61f860e7f
Florian REY <florianrey.info@gmail.com>
2023-02-15 11:17:05 +0100

    add flag

--- 000_initiation_programmation_cpp/015_note_average/main.cpp
+++ 000_initiation_programmation_cpp/015_note_average/main.cpp
@@ -9,19 +9,24 @@ using namespace std;
 int main() {
     double note, sum(0.);
     int num_notes;
+    bool is_invalid;
 
-    cout << "Entrez le nombre de notes : ";
-    cin >> num_notes;
-
-    if (num_notes > 0) {
-        for (int i(1); i <= num_notes; ++i) {
-            cout << "Entrez la note n° " << i << " : ";
-            cin >> note;
-            sum += note;
+    do {
+        cout << "Entrez le nombre de notes : ";
+        cin >> num_notes;
+        is_invalid = num_notes <= 0;
+        if(is_invalid){
+            cout << "Le nombre de notes doit être positif" << endl;
         }
+    } while (is_invalid);
 
-        cout << "Average : " << sum / num_notes << endl;
+    for (int i(1); i <= num_notes; ++i) {
+        cout << "Entrez la note n° " << i << " : ";
+        cin >> note;
+        sum += note;
     }
+
+    cout << "Average : " << sum / num_notes << endl;
 
     return 0;
 }
