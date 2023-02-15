0da04dc0a4b06c85ec290c07c918b87385408ed3
Florian REY <florianrey.info@gmail.com>
2023-02-15 12:11:01 +0100

    variable scope

---
+++ 000_initiation_programmation_cpp/017_scope/main.cpp
@@ -0,0 +1,21 @@
+//
+// Created by Florian Rey on 15/02/2023.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int j(123);
+    cout << "j is " << j << endl;
+    {
+        int j(321);
+        cout << "j is " << j << endl;
+    }
+    j += 100;
+    cout << "j is " << j << endl;
+
+    return 0;
+}
