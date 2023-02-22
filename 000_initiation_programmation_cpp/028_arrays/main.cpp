c8cc6f0cc06890dba44155ad18d046a8f3087df0
florianwns <florianrey.info@gmail.com>
2023-02-22 10:20:22 +0100

    add vec function

--- 000_initiation_programmation_cpp/028_arrays/main.cpp
+++ 000_initiation_programmation_cpp/028_arrays/main.cpp
@@ -18,6 +18,11 @@ int main() {
         } while (age <= 0);
     }
 
+    // Ajoute une element
+    ages.push_back(123);
+    //retire un element
+    ages.pop_back();
+
     // calcul de la moyenne
     double sum(0.0);
     for (size_t i(0); i < ages.size(); ++i) {
@@ -26,6 +31,14 @@ int main() {
     const double avg(sum / ages.size());
 
     cout << "la moyenne d'age est de : " << avg << endl;
+
+    // Ajoute une element
+    cout << "Premier élément : " << ages.front() << endl;
+    cout << "Dernier élément : " << ages.back() << endl;
+    cout << "Est vide : " << ages.empty() << endl;
+
+    // Supprime tous les éléments
+    ages.clear();
 
     return 0;
 }
