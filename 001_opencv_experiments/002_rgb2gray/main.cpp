cd02e37e85b5d69712a03b837f0c3dc4f0a50d6d
Florian REY <florianrey.info@gmail.com>
2023-02-24 16:09:03 +0100

    switch proc

---
+++ 001_opencv_experiments/002_rgb2gray/main.cpp
@@ -0,0 +1,85 @@
+//
+// Created by Florian Rey on 24/02/2023.
+//
+
+#include "opencv2/core.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/highgui.hpp"
+
+#include <iostream>
+
+using namespace std;
+
+#define KEY_UP 0
+#define KEY_DOWN 1
+#define KEY_LEFT 2
+#define KEY_RIGHT 3
+#define KEY_Q 113
+
+
+enum processings {
+    PROC_NONE,
+    PROC_CV_RGB2GRAY,
+    PROC_HOMEMADE_RGB2GRAY,
+    PROC_LENGTH,
+};
+
+int main() {
+    cv::VideoCapture cam;
+    const int device_id = 0;        // 0 = open default camera
+
+    // specify the cv::VideoCaptureAPIs
+    const int api = cv::CAP_ANY;    // 0 = autodetect
+
+    // Open selected camera
+    cam.open(device_id, api);
+
+    // Check if camera is opened
+    if (!cam.isOpened()) {
+        cerr << "Unable to open camera" << endl;
+        return -1;
+    }
+
+    int proc = PROC_NONE;
+
+    // Grab frame
+    cv::Mat frame, res;
+    cout << "Start grabbing" << endl
+         << "Press 'q' to terminate" << endl;
+
+    while (true) {
+        // Wait for a new frame from camera and store it into 'frame'
+        cam.read(frame);
+
+        // Check if we succeeded
+        if (frame.empty()) {
+            cerr << "Blank frame grabbed" << endl;
+            break;
+        }
+
+        switch (proc) {
+            case PROC_NONE:
+                frame.copyTo(res);
+                break;
+        }
+
+        // Show live and wait for a key with timeout long enough to show images
+        cv::imshow("Live", res);
+
+        // Wait 'q' key to terminate
+        switch (cv::waitKey(5)) {
+            case KEY_Q:
+                return 0;
+            case KEY_UP:
+            case KEY_LEFT:
+                proc = (proc + 1) % PROC_LENGTH;
+                break;
+            case KEY_RIGHT:
+            case KEY_DOWN:
+                proc = (proc - 1) % PROC_LENGTH;
+                break;
+        }
+    }
+
+    return 0;
+}
